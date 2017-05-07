3847bcbcbb91d898a4d728edc5f9b0e1b63589fe
Chamith <chamith@sp-solutions.biz>
2017-05-07 10:26:43 +0530

    -employee model

---
+++ src/main/java/com/qualitycoder/payrollhr/model/Employee.java
@@ -0,0 +1,90 @@
+package com.qualitycoder.payrollhr.model;
+
+import java.util.Date;
+
+/**
+ * @author Chamith
+ */
+public class Employee {
+    private String empID;
+    private String empName;
+    private String gender;
+    private Date dob;
+    private String designation;
+    private Double basicSalary;
+    private Double epfContribution;
+    private String phoneNo;
+    private String address;
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(Double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public Double getEpfContribution() {
+        return epfContribution;
+    }
+
+    public void setEpfContribution(Double epfContribution) {
+        this.epfContribution = epfContribution;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
